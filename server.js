3be12cd4e08bddcc9cd510c61d5dbd4c9643f635
pmgirardi <117805446+pmgirardi@users.noreply.github.com>
2023-04-21 14:21:53 -0700

    fixed some syntax errors

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
-const activity = cwd.includes('NoSql-Social-Network-Api')
-  ? cwd.split('/NoSql-Social-Network-Api/')[1]
+const activity = cwd.includes('nosql-social-network-api')
+  ? cwd.split('/nosql-social-network-api/')[1]
   : cwd;
 
 app.use(express.urlencoded({ extended: true }));
